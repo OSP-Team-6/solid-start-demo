b945d9ee61e66d87315aaa5a8d8ae432e9fc798f
Chris <chrisnash819@yahoo.com>
2024-10-02 19:51:23 -0700

    testing library import

--- src/lib/index.ts
+++ src/lib/index.ts
@@ -1,8 +1,18 @@
 import { action, cache, redirect } from '@solidjs/router';
 import { db } from './db';
-import { AuthCallbacks } from '~/auth-lib/authTypes';
-import { authCallbacks } from '~/auth-lib/authCallbacks';
+// import { AuthCallbacks } from '~/auth-lib/authTypes';
+// import { authCallbacks } from '~/auth-lib/authCallbacks';
+import {
+  createAuthCallbacks,
+  type AuthCallbacks,
+} from '@solid-auth/solidstart-auth-backend';
+import { useSession } from 'vinxi/http';
 
+
+const authCallbacks: AuthCallbacks = createAuthCallbacks(useSession);
+
+const userLookupFunction = (username: string) => db.user.findUnique({ where: { username: username } });
+const userCreateFunction = (username:string, password:string) => db.user.create({data: { username, password}})
 // This file runs on the server. These are basically auth server functions. Could be called authServer.ts.
 
 export const getUser = cache(async () => {
@@ -38,8 +48,8 @@ async function performLoginOrRegister(formData: FormData, callbacks: AuthCallbac
 
   try {
     const user = await (loginType !== 'login'
-      ? callbacks.register(username, password)
-      : callbacks.login(username, password));
+      ? callbacks.register(username, password, userLookupFunction, userCreateFunction)
+      : callbacks.login(username, password, userLookupFunction));
     const session = await callbacks.getSession();
     await session.update((d) => {
       d.userId = user.id.toString();
