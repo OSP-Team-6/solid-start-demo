18113b044bd525c08987e690ad8fb17980d9b941
Ryan Gilbert <ryanbgilbert@gmail.com>
2024-10-21 21:32:38 -0700

    Import Bcrypt and test support for either supplying it or not

--- src/lib/server.ts
+++ src/lib/server.ts
@@ -1,6 +1,9 @@
 import { createAuthCallbacks } from '@solid-auth/solidstart-auth-backend';
 import { useSession } from 'vinxi/http';
+import bcrypt from 'bcrypt';
 
-export const authCallbacks = createAuthCallbacks(useSession);
+// Example: Without hashing library
+// export const authCallbacks = createAuthCallbacks(useSession);
 
-//this had the server fucntions(login,logout etc)
+// Example: With hashing library
+export const authCallbacks = createAuthCallbacks(useSession, bcrypt);
