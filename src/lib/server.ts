e03511e77eb22b143b3541fde4e5388073a6fc43
Ryan Gilbert <ryanbgilbert@gmail.com>
2024-10-22 18:04:29 -0700

    Pass bcrypt as hashing function

--- src/lib/server.ts
+++ src/lib/server.ts
@@ -2,8 +2,18 @@ import { createAuthCallbacks } from '@solid-auth/solidstart-auth-backend';
 import { useSession } from 'vinxi/http';
 import bcrypt from 'bcrypt';
 
-// Example: Without hashing library
+// Example: With no hashing functions declared
 // export const authCallbacks = createAuthCallbacks(useSession);
 
-// Example: With hashing library
-export const authCallbacks = createAuthCallbacks(useSession, bcrypt);
+// Example: With hashing functions declared
+// Using bcrypt
+const hashingFunctions = {
+  hash: async (password: string) => {
+    return bcrypt.hash(password, 12);
+  },
+  compare: async (password: string, hashedPassword: string) => {
+    return bcrypt.compare(password, hashedPassword);
+  },
+};
+
+export const authCallbacks = createAuthCallbacks(useSession, hashingFunctions);
